db37ef8bdc2bc0c6a71963b474da79a8f770951b
Jordan Gonzales <201614047@uns.edu.pe>
2019-09-20 18:04:18 -0500

    MVC-Java TXT v0.0.1

--- src/com/demo/model/ModeloUsuario.java
+++ src/com/demo/model/ModeloUsuario.java
@@ -1,6 +1,9 @@
 package com.demo.model;
 
 import com.demo.model.entity.Usuario;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ModeloUsuario extends Model {
     
@@ -12,6 +15,23 @@ public class ModeloUsuario extends Model {
     
     static public long logUp(Usuario user){
         long status = 0;
+        FileWriter flwriter = null;
+        try {
+            flwriter = new FileWriter("C:\\db\\usuarios.txt");
+            BufferedWriter bfwriter = new BufferedWriter(flwriter);
+            bfwriter.write(user.getId().toString() + ", " + user.getNombres() + ""  + "\n");
+            bfwriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (flwriter != null) {
+                try {
+                    flwriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         System.out.println(user);
         return status;
     }
